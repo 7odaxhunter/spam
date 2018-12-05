b27e5d8cf6041405925eec8d13456aa6e39f8f08
7odaxhunter <45143494+7odaxhunter@users.noreply.github.com>
2018-12-05 05:41:46 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,100 +1,34 @@
-﻿const Discord = require("discord.js");
+﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Discord.Client();
+var prefix = "!"; // البيرفكس حطها بين ال " اقترح تخليها زي ما هي
 
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Kahrbaa `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+  console.log(`Welcome Bro ${client.user.tag}!`);  // لا تعدل شي هنا
 });
 
+client.on('ready', async() => { // ولا تعدل شي في اي مكان غير تحت
+var server = "509357857666236430"; // ايدي السررفر اللي بتحط فيه البوت
+var channel = "509357857666236434";//ايدي الروم اللي بسوي فيه سبام البوت
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('```INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ INCR_iSryManYT SPAAAAM ~ ') 
+    },305);
+})
 
 client.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
-});
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
-});
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
-client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
-});
+  let args = message.content.split(" ").slice(1);
 
-client.on('message', message => {
-    if(message.content === '-ببب'){
-        message.channel.send('#credits @!《SK》7odaX3Unter#6629 200')
-    }
-});
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ }); // لا تعدل شي باي مكان
 
-client2.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === '-ببب'){
-        message.channel.send('#credits @!《SK》7odaX3Unter#6629 200')
-    }
-});
-
-
-
-client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+ 
+client.login(process.env.BOT_TOKEN); // هنا لا تعدل شي
